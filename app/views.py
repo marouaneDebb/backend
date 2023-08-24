b84b80879d5ebacc39a503509ebe45e22439de6c
marouaneDebb <marwanedebb@gmail.com>
2023-08-24 01:44:20 +0100

    update sign up and start working on sign in (bakend)

--- app/views.py
+++ app/views.py
@@ -3,21 +3,47 @@ from rest_framework.views import APIView
 from . models import *
 from rest_framework.response import Response
 from . serializer import *
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import React
+from django.contrib.auth.models import User
+
 # Create your views here.
 
-
+# Define a class-based view for React model
 class ReactView(APIView):
 
     serializer_class = ReactSerializer
 
+    # Handle GET request
     def get(self, request):
+        # Fetch all React objects and serialize them
         output = [{"email": output.email, "password": output.password}
                   for output in React.objects.all()]
         return Response(output)
 
+    # Handle POST request
     def post(self, request):
-
         serializer = ReactSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+
+# Define a view to check user account
+@csrf_exempt
+def check_user(request):
+    if request.method == 'POST':
+        data = request.POST  # or request.data for JSON data
+        email = data.get('email')
+        password = data.get('password')
+
+        try:
+            # Check if user exists with the given email and matching password
+            user = React.objects.get(email=email)
+            if user.password == password:
+                return JsonResponse({'match': True})
+        except React.DoesNotExist:
+            pass
+
+    return JsonResponse({'match': False})
+
--- backend/urls.py
+++ backend/urls.py
@@ -1,24 +1,15 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
-#from django.conf.urls import url
-from app.views import *
+from django.urls import path
+from app.views import ReactView, check_user
 
+# URL patterns for your Django app
 urlpatterns = [
+    # Admin panel URL
     path('admin/', admin.site.urls),
+    
+    # Default route for ReactView
     path('', ReactView.as_view(), name="xxx"),
+    
+    # API endpoint to check user account
+    path('api/check-user', check_user, name='check_user'),
 ]
